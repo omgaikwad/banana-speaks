ffee407ebd36ed28592a03c29977a2b66089832b
Om Gaikwad <62072438+omgaikwad@users.noreply.github.com>
2021-07-10 16:19:24 +0530

    updated js

---
+++ app.js
@@ -0,0 +1,25 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("Error occured ", error);
+}
+
+var a = "dont click twice";
+function clickEventHandler(){
+    fetch(getTranslationURL(txtInput.value))
+    .then(response => response.json())
+    .then(json => txtOutput.innerText = (json.contents.translated))
+    .catch(errorHandler)
+
+};
+
+btnTranslate.addEventListener("click" , clickEventHandler);
